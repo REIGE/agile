0b004009e377d1ec3044fec010c39b99884be252
reige <407601459@qq.com>
2018-07-15 00:19:50 +0800

    初始化项目

---
+++ agile-common/src/main/java/com/agile/common/annotation/BaseService.java
@@ -0,0 +1,13 @@
+package com.agile.common.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author chenrui
+ * @date 2018/7/10.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface BaseService {
+}
